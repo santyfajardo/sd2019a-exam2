bc8e280318a25b139a5a07de52bd54694ac3a462
santyfajardo <santiago_fajardo96@hotmail.com>
2019-04-23 13:30:11 -0500

    Cambio de estrategia

--- Database-Web/agregar.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO estudiantes VALUES (13, 'Phoenix', 'AZ', 'ab');
-INSERT INTO estudiantes VALUES (44, 'Denver', 'CO', 'hola');
-INSERT INTO estudiantes VALUES (66, 'Caribou', 'ME', 'adios'); 
