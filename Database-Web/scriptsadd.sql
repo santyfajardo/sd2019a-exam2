15761d317f6dc24ab6cb33abf9d511c86bca65d4
santyfajardo <santiago_fajardo96@hotmail.com>
2019-04-23 14:05:09 -0500

    Update scrip add

--- Database-Web/scriptsadd.sql
+++ Database-Web/scriptsadd.sql
@@ -1,1 +1,5 @@
-INSERT INTO employees (first_name, last_name, department, email) VALUES ('Lorenz', 'Vanthillo', 'IT', 'lvthillo@mail.com');
+INSERT INTO employees VALUES ('Lorenz', 'Vanthillo', 'IT', 'lvthillo@mail.com');
+INSERT INTO employees VALUES ('Santiago', 'Fajardo', 'IT', 'santiago@mail.com');
+INSERT INTO employees VALUES ('Julian', 'Nino', 'IT', 'juian@mail.com');
+INSERT INTO employees VALUES ('Daniel', 'Perez', 'IT', 'daniel@mail.com');
+INSERT INTO employees VALUES ('Felipe', 'jurado', 'IT', 'felipe@mail.com');
