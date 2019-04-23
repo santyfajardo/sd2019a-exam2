43054e13da4b5190ee4aab9695b7af2ad615d83a
santyfajardo <santiago_fajardo96@hotmail.com>
2019-04-23 14:19:28 -0500

    Version estable basededatos

--- Database-Web/scriptsadd.sql
+++ Database-Web/scriptsadd.sql
@@ -1,5 +1,5 @@
-INSERT INTO employees VALUES ('Lorenz', 'Vanthillo', 'IT', 'lvthillo@mail.com');
-INSERT INTO employees VALUES ('Santiago', 'Fajardo', 'IT', 'santiago@mail.com');
-INSERT INTO employees VALUES ('Julian', 'Nino', 'IT', 'juian@mail.com');
-INSERT INTO employees VALUES ('Daniel', 'Perez', 'IT', 'daniel@mail.com');
-INSERT INTO employees VALUES ('Felipe', 'jurado', 'IT', 'felipe@mail.com');
+INSERT INTO alumnos VALUES ('Lorenz', 'Vanthillo', 'IT', 'lvthillo@mail.com');
+INSERT INTO alumnos VALUES ('Santiago', 'Fajardo', 'IT', 'santiago@mail.com');
+INSERT INTO alumnos VALUES ('Julian', 'Nino', 'IT', 'juian@mail.com');
+INSERT INTO alumnos VALUES ('Daniel', 'Perez', 'IT', 'daniel@mail.com');
+INSERT INTO alumnos VALUES ('Felipe', 'jurado', 'IT', 'felipe@mail.com');
