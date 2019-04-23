94d226039ffdf02ee4089fa7b799b130f7a60966
santyfajardo <santiago_fajardo96@hotmail.com>
2019-04-23 14:08:46 -0500

    Update scrip create table

--- Database-Web/scripts.sql
+++ Database-Web/scripts.sql
@@ -1,6 +1,6 @@
-CREATE TABLE employees (
-first_name varchar(25),
-last_name  varchar(25),
-department varchar(15),
+CREATE TABLE alumnos (
+nombre varchar(25),
+apellido  varchar(25),
+carrera varchar(15),
 email  varchar(50)
 );
