4a832b55a2c21201bac4f7e63309db513c161e6b
santyfajardo <santiago_fajardo96@hotmail.com>
2019-04-30 16:14:12 -0500

    Corrección script de generación de logs

--- script3.sh
+++ script3.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 while true
 do
-	x= `$(($RANDOM%2))`
-	if [$x -eq 1]
-	then
+
   		curl 192.168.130.160:8080
 		sleep 2
-	else 
 		curl 192.168.130.160:8080/mala
 		sleep 2
-	fi
+
 done
