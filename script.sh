9c5d9248746d7fc0b63adfb374e446fe0d1ff740
StevenMontealegre <james.montealegre@correo.icesi.edu.co>
2019-04-29 20:05:07 -0500

    Creacion de script para generacion de logs

--- script.sh
+++ script.sh
@@ -1,2 +1,13 @@
-watch -n 1 curl 127.0.0.1 -X GET | grep a
-
+#!/bin/bash
+while true
+do
+	x= `echo $(($RANDOM%1))`
+	if [$x= 1]
+	then
+  		watch -n 1 curl 192.168.130.160:3306 -X GET
+		sleep 2
+	else 
+		watch -n 1 curl 192.168.130.160:3306/mala -X GET]
+		sleep 2
+	fi
+done
---
+++ script2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Esto es un ejemplo en Bash del clasico "Hola Mundo"
+#
+
+MENSAJE="Hola Mundo"
+echo $MENSAJE
+
